9ef98e086bf4e0b129ac480a1101c9d025d109a3
Andromeda <hannalauth@gmail.com>
2019-03-19 17:22:30 -0700

    solved

--- reverseAString.js
+++ reverseAString.js
@@ -1,3 +1,15 @@
 function reverseAString(str) {
 
+    var reverseStr = [];
+
+    for (var i = str.length - 1; 0 <= i; i--) {
+        reverseStr.push(str[i]);
+    }
+
+    console.log(reverseStr.join(''));
+    return reverseStr.join('');
+ 
 }
+
+var str = "foobar";
+reverseAString(str);
