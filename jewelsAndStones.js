b0cc4e083e70c9e98313aa434cadb7be6eb3bf69
Andromeda <hannalauth@gmail.com>
2019-03-23 17:20:58 -0700

    solved

--- jewelsAndStones.js
+++ jewelsAndStones.js
@@ -1,3 +1,31 @@
 function JewelsAndStones(j, s) {
+    
+    // initialize object that stores key value pair (or key & count pairs) count of each jewel
+    var treasureBox = {};
+    var totalJewels = 0
+    // iterate i over jewel string 
+    // push each jewel into an object and set count to 0
+    for (var i = 0; i < j.length; i++) {
+        treasureBox[j[i]] = 0
+    }
+    
+    // iterate i over stone string
+        // if i is equal to var key in object then increment object[key] by 1
+    for (var i = 0; i < s.length; i++) {
+        if (s[i] in treasureBox) {
+            treasureBox[s[i]]++
+        }
+    }
+
+    // add both object[key] values together 
+    for (var key in treasureBox) {
+        totalJewels += parseInt(treasureBox[key])      
+    }
+        console.log(totalJewels)
+        return totalJewels
 
 }
+
+var j = "aA";
+var s = "aAAbbb";
+JewelsAndStones(j,s);
