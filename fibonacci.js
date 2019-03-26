e14664c928a667f2bd65717e4ab05744b490ec3f
Andromeda <hannalauth@gmail.com>
2019-03-25 18:57:44 -0700

    actual javascript logic solve

--- fibonacci.js
+++ fibonacci.js
@@ -1,27 +1,52 @@
 function fibonacciASequenceFoundInNature(whichFib) {
-    
-    var fib = whichFib - 1;
-    
-     if (whichFib === 1) {
-         return 0
-     } else {
-        
-        var goldenRatio = ((Math.pow(1.61803398874989484820, fib)) - (Math.pow(-0.61803398874989484820, fib)));
-        var squareRootFive = Math.sqrt(5);
-        var getWhichFib = (goldenRatio/squareRootFive);
 
-        console.log(getWhichFib);
-         
-     }
-    
-    return Math.round(getWhichFib)
-    
+    var indexOfFib = whichFib - 1;
+    var arr = [0,1];
+    var temp = 0;
+    var value = 0;
+
+    for (var i = 0; i < indexOfFib; i++) {
+        temp = parseInt(arr[i]) + parseInt(arr[i+1]);
+        arr.push(temp);
+        }
+        console.log(arr)
+    for (var i = 0; i < arr.length; i++) {
+        if (i === indexOfFib) {
+            console.log(arr[i])
+            value = arr[i]
+        }
+    }
+    console.log(value)
+    return value
 }
 
-fibonacciASequenceFoundInNature(3);
-fibonacciASequenceFoundInNature(6);
-fibonacciASequenceFoundInNature(25);
-fibonacciASequenceFoundInNature(40);
+fibonacciASequenceFoundInNature(5);
+fibonacciASequenceFoundInNature(7);
+
+// function fibonacciASequenceFoundInNature(whichFib) {
+    
+//     var fib = whichFib - 1;
+    
+//      if (whichFib === 1) {
+//          return 0
+//      } else {
+        
+//         var goldenRatio = ((Math.pow(1.61803398874989484820, fib)) - (Math.pow(-0.61803398874989484820, fib)));
+//         var squareRootFive = Math.sqrt(5);
+//         var getWhichFib = (goldenRatio/squareRootFive);
+
+//         console.log(getWhichFib);
+         
+//      }
+    
+//     return Math.round(getWhichFib)
+    
+// }
+
+// fibonacciASequenceFoundInNature(3);
+// fibonacciASequenceFoundInNature(6);
+// fibonacciASequenceFoundInNature(25);
+// fibonacciASequenceFoundInNature(40);
 
 // 3 > 2
 // 6 > 8
