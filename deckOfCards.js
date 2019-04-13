d77eb2523ccd668a0ab948abd32e21e94e83bcdc
Andromeda <hannalauth@gmail.com>
2019-04-13 09:43:51 -0700

    new problem

---
+++ deckOfCards.js
@@ -0,0 +1,248 @@
+var deckOfCards = {
+    cards = [
+        {
+            suit: "diamond",
+            value: "ace"
+        },
+        {
+            suit: "heart",
+            value: "ace"
+        },
+        {
+            suit: "spade",
+            value: "ace"
+        },
+        {
+            suit: "club",
+            value: "ace"
+        },
+        {
+            suit: "diamond",
+            value: "10"
+        },
+        {
+            suit: "heart",
+            value: "10"
+        },
+        {
+            suit: "spade",
+            value: "10"
+        },
+        {
+            suit: "club",
+            value: "10"
+        },
+        {
+            suit: "diamond",
+            value: "king"
+        },
+        {
+            suit: "heart",
+            value: "king"
+        },
+        {
+            suit: "spade",
+            value: "king"
+        },
+        {
+            suit: "club",
+            value: "king"
+        },
+        {
+            suit: "diamond",
+            value: "queen"
+        },
+        {
+            suit: "heart",
+            value: "queen"
+        },
+        {
+            suit: "spade",
+            value: "queen"
+        },
+        {
+            suit: "club",
+            value: "queen"
+        },
+        {
+            suit: "diamond",
+            value: "jack"
+        },
+        {
+            suit: "heart",
+            value: "jack"
+        },
+        {
+            suit: "spade",
+            value: "jack"
+        },
+        {
+            suit: "club",
+            value: "jack"
+        },
+        {
+            suit: "diamond",
+            value: "10"
+        },
+        {
+            suit: "heart",
+            value: "10"
+        },
+        {
+            suit: "spade",
+            value: "10"
+        },
+        {
+            suit: "club",
+            value: "10"
+        },
+        {
+            suit: "diamond",
+            value: "9"
+        },
+        {
+            suit: "heart",
+            value: "9"
+        },
+        {
+            suit: "spade",
+            value: "9"
+        },
+        {
+            suit: "club",
+            value: "9"
+        },
+        {
+            suit: "diamond",
+            value: "8"
+        },
+        {
+            suit: "heart",
+            value: "8"
+        },
+        {
+            suit: "spade",
+            value: "8"
+        },
+        {
+            suit: "club",
+            value: "8"
+        },
+        {
+            suit: "diamond",
+            value: "7"
+        },
+        {
+            suit: "heart",
+            value: "7"
+        },
+        {
+            suit: "spade",
+            value: "7"
+        },
+        {
+            suit: "club",
+            value: "7"
+        },
+        {
+            suit: "diamond",
+            value: "6"
+        },
+        {
+            suit: "heart",
+            value: "6"
+        },
+        {
+            suit: "spade",
+            value: "6"
+        },
+        {
+            suit: "club",
+            value: "6"
+        },
+        {
+            suit: "diamond",
+            value: "5"
+        },
+        {
+            suit: "heart",
+            value: "5"
+        },
+        {
+            suit: "spade",
+            value: "5"
+        },
+        {
+            suit: "club",
+            value: "5"
+        },
+        {
+            suit: "diamond",
+            value: "4"
+        },
+        {
+            suit: "heart",
+            value: "4"
+        },
+        {
+            suit: "spade",
+            value: "4"
+        },
+        {
+            suit: "club",
+            value: "4"
+        },
+        {
+            suit: "diamond",
+            value: "3"
+        },
+        {
+            suit: "heart",
+            value: "3"
+        },
+        {
+            suit: "spade",
+            value: "3"
+        },
+        {
+            suit: "club",
+            value: "3"
+        },
+        {
+            suit: "diamond",
+            value: "2"
+        },
+        {
+            suit: "heart",
+            value: "2"
+        },
+        {
+            suit: "spade",
+            value: "2"
+        },
+        {
+            suit: "club",
+            value: "2"
+        },
+    ],
+
+    shuffle = function() {
+
+    },
+
+    drawFromTop = function() {
+
+    },
+
+    drawRandom = function() {
+
+    },
+
+    insertOnBottom = function() {
+
+    },
+
+    insertRandom = function() {
+
+    }
+}
